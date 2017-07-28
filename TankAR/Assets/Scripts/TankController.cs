99c686260bee624be2b75d031d111372ed90ee3c
Ryan Alameddine <rhalameddine@gmail.com>
2017-07-28 11:39:13 -0700

    Added second Tank

--- TankAR/Assets/Scripts/TankController.cs
+++ TankAR/Assets/Scripts/TankController.cs
@@ -9,15 +9,18 @@ public class TankController : MonoBehaviour {
     public float offsetX = 1;
     public float offsetY = 1;
 
+    [SerializeField]
+    bool tankTwo = false;
+
     private void Start()
     {
         Udp = GameObject.FindGameObjectWithTag("GameController").GetComponent<UdpController>();   
     }
 
     void Update () {
-        x = Mathf.Lerp(-6, 5, Mathf.InverseLerp(150, 1171, Udp.x1)) + offsetX;
-        y = Mathf.Lerp(-3, 2, Mathf.InverseLerp(654, 160, Udp.y1)) + offsetY;
+        x = Mathf.Lerp(-6, 5, Mathf.InverseLerp(150, 1171, tankTwo ? Udp.x2 : Udp.x1)) + offsetX;
+        y = Mathf.Lerp(-3, 2, Mathf.InverseLerp(654, 160, tankTwo ? Udp.y2 : Udp.y1)) + offsetY;
         transform.position = new Vector2(x, y);
-        transform.rotation = Quaternion.Euler(0, 0, Udp.angle);
+        transform.rotation = Quaternion.Euler(0, 0, tankTwo ? Udp.angle2 : Udp.angle);
 	}
 }
