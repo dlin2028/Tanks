3c1879c53a86bec6dd91fe17cd467e8125589ff5
Ryan Alameddine <rhalameddine@gmail.com>
2017-07-28 12:59:42 -0700

    Finished Turret tracking and THE SCENE SHOULD WORK NOW IF YOU DONT MESS UP MERGE CONFLICTS

--- TankAR/Assets/Scripts/TurretController.cs
+++ TankAR/Assets/Scripts/TurretController.cs
@@ -10,13 +10,21 @@ public class TurretController : MonoBehaviour {
 	}
 
 	void Update () {
-		if(Vector3.Distance(transform.position, Tanks[0].transform.position) > Vector3.Distance(transform.position, Tanks[1].transform.position))
+		if(Vector3.Distance(transform.position, Tanks[1].transform.position) > Vector3.Distance(transform.position, Tanks[0].transform.position))
         {
+            Vector3 dist = transform.position - Tanks[0].transform.position;
+            dist.Normalize();
 
+            float z = Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, z - 90);
         }
         else
         {
+            Vector3 dist = transform.position - Tanks[1].transform.position;
+            dist.Normalize();
 
+            float z = Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, z - 90);
         }
 	}
 }
