7b6cf5d6b48a3dca5d82bb547f53b377ee3cbffe
David Lin <letterman2002@gmail.com>
2017-07-28 14:24:57 -0700

    bullets

---
+++ TankAR/Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public KeyCode key;
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            GameObject createdObject = Instantiate(bullet);
+            createdObject.transform.position = transform.position;
+            createdObject.transform.rotation = transform.rotation;
+        }
+	}
+}
---
+++ TankAR/Assets/Scripts/StartWithVelocity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StartWithVelocity : MonoBehaviour {
+
+    public Vector3 velocity;
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(velocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
